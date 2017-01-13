26292ad0d0d4b0559d89f7927487fdd277fead2d
Caleb Porzio <caleb@fmtc.co>
2017-01-13 16:54:14 -0500

    added some more unit tests to cover more of the system

--- tests/OnbaordTest.php
+++ tests/OnbaordTest.php
@@ -5,15 +5,47 @@ use Calebporzio\Onboard\OnboardingManager;
 
 class OnboardTest extends PHPUnit_Framework_TestCase
 {
+	/** 
+	 * Dummy user object.
+	 */
+	protected $user;
+
+	public function setUp()
+	{
+		$this->user = $this->getMock('User');
+	}
+
+	/** @test */
+	public function steps_can_be_defined_and_configured()
+	{
+		$onboardingSteps = new OnboardingSteps;
+
+		$onboardingSteps->addStep('Test Step')
+	    	->link('/some/url')
+	    	->cta('Test This!')
+	    	->attributes(['another' => 'attribute'])
+	    	->completeIf(function () {
+	    		return true;
+	    	});
+
+	    $this->assertEquals(1, $onboardingSteps->steps(new stdClass())->count());
+
+	    $step = $onboardingSteps->steps(new stdClass())->first();
+
+	    $this->assertEquals('/some/url', $step->link);
+	    $this->assertEquals('Test This!', $step->cta);
+	    $this->assertEquals('Test Step', $step->title);
+	    $this->assertEquals('attribute', $step->another);
+	}
+
 	/** @test */
 	public function is_in_progress_when_all_steps_are_incomplete()
 	{
 		$onboardingSteps = new OnboardingSteps;
-
 		$onboardingSteps->addStep('Test Step');
 		$onboardingSteps->addStep('Another Test Step');
 
-		$onboarding = new OnboardingManager(new stdClass(), $onboardingSteps);
+		$onboarding = new OnboardingManager($this->user, $onboardingSteps);
 
 		$this->assertTrue($onboarding->inProgress());
 		$this->assertFalse($onboarding->finished());
@@ -23,18 +55,34 @@ class OnboardTest extends PHPUnit_Framework_TestCase
 	public function is_finished_when_all_steps_are_complete()
 	{
 		$onboardingSteps = new OnboardingSteps;
-
 		$onboardingSteps->addStep('Test Step')
 			->completeIf(function() { 
 				return true; 
 			});
 
-		$onboardingSteps->addStep('Another Test Step')
-			->completeIf(function() { return true; });
-
-		$onboarding = new OnboardingManager(new stdClass(), $onboardingSteps);
+		$onboarding = new OnboardingManager($this->user, $onboardingSteps);
 
 		$this->assertTrue($onboarding->finished());
 		$this->assertFalse($onboarding->inProgress());
+	}
+
+	/** @test */
+	public function the_proper_object_gets_passed_into_completion_callback()
+	{
+		$user = $this->getMock('User', ['testMe']);
+		$user->expects($this->once())->method('testMe');
+
+		$onboardingSteps = new OnboardingSteps;
+		$onboardingSteps->addStep('Test Step')
+			->completeIf(function($user) { 
+				// if this gets called, it ensures that the right object was passed through.
+				$user->testMe();
+				return true; 
+			});
+
+		$onboarding = new OnboardingManager($user, $onboardingSteps);
+
+		// Calling finished() will triger the completeIf callback.
+		$this->assertTrue($onboarding->finished());
 	}
 }
